d08f6480103836ef0d74aaf559243203bc24caff
unknown <rizky.darmarazak@gmail.com>
2021-04-12 16:47:50 +0700

    test

---
+++ src/elements/DarkTheme.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export default function DarkTheme() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  function onClick() {
+    document.body.classList.toggle("dark-theme");
+    setDarkTheme(!darkTheme);
+  }
+
+  return (
+    <div className="theme" id="theme" onClick={() => onClick()}>
+      {darkTheme ? (
+        <i className="icon bi bi-moon-fill"></i>
+      ) : (
+        <i className="icon bi bi-sun-fill"></i>
+      )}
+    </div>
+  );
+}
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,11 +8,14 @@ import Projects from "parts/Projects";
 import Contact from "parts/Contact";
 import Footer from "parts/Footer";
 
+import DarkTheme from "elements/DarkTheme";
+
 import landingPage from "json/landingPage.json";
 
 export default function LandingPage() {
   return (
     <>
+      <DarkTheme></DarkTheme>
       <Header></Header>
       <Hero data={landingPage.hero}></Hero>
       <Service data={landingPage.service}></Service>
